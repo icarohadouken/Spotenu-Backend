502b94b216f3bcc341d1583c797c643c35802210
icarohadouken <icaro.ssantos96@gmail.com>
2020-08-14 14:52:00 -0300

    testando de novo o heroku

--- src/local.ts
+++ src/local.ts
@@ -1,11 +1,1 @@
 import { app } from "./index";
-import { AddressInfo } from "net";
-
-const server = app.listen(process.env.PORT || 3006, () => {
-    if (server) {
-        const address = server.address() as AddressInfo
-        console.log(`Server is runing in http://localhost:${address.port}`)
-    } else {
-        console.error(`Server Failure`)
-    }
-})
